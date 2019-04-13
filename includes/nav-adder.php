61bbe82e280236edeef8c26c7ef6dc4d0e6f5d12
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-04-13 23:08:40 +0600

    Run loop only if posts exist

--- includes/nav-adder.php
+++ includes/nav-adder.php
@@ -18,42 +18,46 @@ function bp_nav_adder() {
 		)
 	);
 
-	foreach ( $bp_custom_menu_pages as $bp_custom_menu_page ) {
-		setup_postdata( $post );
-
-		bp_core_new_nav_item(
-			array(
-				'name' => $bp_custom_menu_page->post_title,
-				'slug' => $bp_custom_menu_page->post_name,
-				'screen_function' => 'bp_custom_menu_page_screen_function',
-				'default_subnav_slug' => $bp_custom_menu_page->post_name,
-			)
-		);
-
-		$bp_custom_sub_menu_pages = get_posts(
-			array(
-				'post_type' => 'bp_custom_menu_page',
-				'post_parent' => $bp_custom_menu_page->ID,
-			)
-		);
-
-		foreach( $bp_custom_sub_menu_pages as $bp_custom_sub_menu_page ) {
+	if ( ! empty ( $bp_custom_menu_pages ) ) {
+		foreach ( $bp_custom_menu_pages as $bp_custom_menu_page ) {
 			setup_postdata( $post );
 
-			bp_core_new_subnav_item(
+			bp_core_new_nav_item(
 				array(
-					'name' => $bp_custom_sub_menu_page->post_title,
-					'slug' => $bp_custom_sub_menu_page->post_name,
-					'parent_slug' => $bp_custom_menu_page->post_name,
-					'parent_url' => $bp->loggedin_user->domain . $bp_custom_menu_page->post_name . '/',
-					'screen_function' => 'bp_custom_sub_menu_screen_function',
+					'name' => $bp_custom_menu_page->post_title,
+					'slug' => $bp_custom_menu_page->post_name,
+					'screen_function' => 'bp_custom_menu_page_screen_function',
+					'default_subnav_slug' => $bp_custom_menu_page->post_name,
 				)
 			);
+
+			$bp_custom_sub_menu_pages = get_posts(
+				array(
+					'post_type' => 'bp_custom_menu_page',
+					'post_parent' => $bp_custom_menu_page->ID,
+				)
+			);
+
+			if ( ! empty ( $bp_custom_sub_menu_pages ) ) {
+				foreach( $bp_custom_sub_menu_pages as $bp_custom_sub_menu_page ) {
+					setup_postdata( $post );
+
+					bp_core_new_subnav_item(
+						array(
+							'name' => $bp_custom_sub_menu_page->post_title,
+							'slug' => $bp_custom_sub_menu_page->post_name,
+							'parent_slug' => $bp_custom_menu_page->post_name,
+							'parent_url' => $bp->loggedin_user->domain . $bp_custom_menu_page->post_name . '/',
+							'screen_function' => 'bp_custom_sub_menu_screen_function',
+						)
+					);
+				}
+				wp_reset_postdata();
+			}
+
 		}
 		wp_reset_postdata();
-
 	}
-	wp_reset_postdata();
 
 }
 add_action( 'bp_setup_nav', 'bp_nav_adder', 302 );
