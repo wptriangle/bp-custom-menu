0253b0a132bb0eab252ed3defea627b22a02b0ea
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-04-18 22:20:50 +0600

    Limit the depth of bp custom menu page to 1

--- includes/plugin-functions.php
+++ includes/plugin-functions.php
@@ -6,3 +6,15 @@
  * @package    BuddyPress Custom Menu
  * @author     Nahid Ferdous Mohit
  */
+
+/*
+ * Set the depth of the hierarchical
+ * bp_custom_menu_page post type to 1
+ */
+
+function set_bp_custom_menu_depth( $a ) {
+  $a['depth'] = 1;
+  return $a;
+}
+add_action( 'page_attributes_dropdown_pages_args','set_bp_custom_menu_depth' );
+add_filter( 'quick_edit_dropdown_pages_args', 'set_bp_custom_menu_depth' );
