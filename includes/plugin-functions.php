cc649d7a73eb61753d038abcf4e8404ce350e43f
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-07-25 20:47:32 +0600

    Limiting depth made specific to menu page post type only

--- includes/plugin-functions.php
+++ includes/plugin-functions.php
@@ -13,8 +13,11 @@
  */
 
 function set_bp_custom_menu_depth( $a ) {
-  $a['depth'] = 1;
-  return $a;
+	global $post;
+	if( $post->post_type == 'bp_custom_menu_page' ) {
+  		$a['depth'] = 1;
+		  return $a;
+	}
 }
 add_action( 'page_attributes_dropdown_pages_args','set_bp_custom_menu_depth' );
 add_filter( 'quick_edit_dropdown_pages_args', 'set_bp_custom_menu_depth' );
